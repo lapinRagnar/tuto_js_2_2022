25d832d84d3c5c6864c14ec70ffcd85ebc45bed9
lapinRagnar <lapinragnar@gmail.com>
2022-02-09 20:15:13 +0100

    methode 1 - validation formulaire

--- 13_valider_des_formulaires/app.js
+++ 13_valider_des_formulaires/app.js
@@ -1,3 +1,49 @@
-document.getElementById('inscription').addEventListener('submit', ()=>{
-    alert('formulaire envoyé')
+document.getElementById('inscription').addEventListener('submit', (e)=>{
+
+    e.preventDefault()
+
+    let erreur
+    // pour eviter de selectionner tous les inputs
+    inputs = document.getElementById('inscription').getElementsByTagName('input')
+    // ou
+    // inputs = this.getElementsByTagName('input')
+    
+    console.log(inputs);
+    for (i=0; i<inputs.length; i++){
+        if (!inputs[i].value){
+            erreur = "Il faut remplir tous les chammps!"
+            console.log(erreur);
+        }
+    }
+
+    if (erreur){
+        e.preventDefault()
+        document.getElementById('erreur').innerHTML = erreur
+
+        setTimeout(() => {
+            location.reload()
+        }, 3000);
+        return false
+    }
+
+    // if (!pseudo.value) {
+    //     erreur = "entrez un pseudo!"
+    // }
+    // if (!email.value) {
+    //     erreur = "entrez un email!"
+    // }
+    // if (!email1.value) {
+    //     erreur = "entrez un email!"
+    // }
+    // if (!mdp.value) {
+    //     erreur = "entrez un mdp!"
+    // }
+
+    // if (erreur){
+    //     e.preventDefault()
+    //     document.getElementById('erreur').innerHTML = erreur
+    //     return false
+    // } else{
+    //     alert('formulaire envoyé!')
+    // }
 })
